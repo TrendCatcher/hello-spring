27c4f79c0d66e6f718977aac33cef6743efe90f4
TrendCatcher <karleo@kyonggi.ac.kr>
2023-07-14 23:52:31 +0900

    update: [study - Spring] Pure JDBC

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,19 +1,34 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig{
+    private DataSource dataSource;
+@Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
     @Bean
     public MemberService memberService(){       //Bean어노테이션은 스프링 빈에 등록하라는 어노테이션이다.
         return new MemberService(memberRepository()); //spring Bean에 등록되어있는 memberRepository를 memerService에 넣어줌
     }
     @Bean
     public MemberRepository memberRepository(){     //MemberRepository는 인터페이스, MemoryMemberRepository는 구현체
-        return new MemoryMemberRepository();
+
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
+
+
 }
